ba399fec415bdffaf0173e2caefc813dbe2de04e
twesigye10 <twesigye10@gmail.com>
2021-04-20 14:01:47 +0300

    fixed piechart variations across tabs

--- app.R
+++ app.R
@@ -29,12 +29,12 @@ df_data<- dat$df_data
 df_shape<- dat$df_shape
 
 beneficiary_types <- df_data %>% 
-    filter(!is.na(Select_Beneficiary_Type)) %>% pull(Select_Beneficiary_Type) %>% unique()
+    filter(!is.na(Select_Beneficiary_Type)) %>% pull(Select_Beneficiary_Type) %>% unique() %>% sort()
 
 # add data food security
 fs_df_data <-dat$fs_df_data
 fs_beneficiary_types <- fs_df_data %>% 
-    filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()
+    filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique() %>% sort()
 
 reach_theme <- bs_theme(
     bg = ggreach::reach_cols("lightgrey"), 

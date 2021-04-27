28810d6d36216fad3d13d64549ab046262d95eb4
twesigye10 <twesigye10@gmail.com>
2021-04-27 11:08:32 +0300

    importing data module

---
+++ modules/import-data.R
@@ -0,0 +1,19 @@
+# Data --------------------------------------------------------------------
+
+display_in_title <- " for all Districts"
+# add data CBI
+dat<-read_rds(file = "data/data.rds")
+df_data<- dat$df_data
+df_shape<- dat$df_shape
+
+beneficiary_types <- df_data %>% 
+  filter(!is.na(Select_Beneficiary_Type)) %>% pull(Select_Beneficiary_Type) %>% unique() %>% sort()
+
+# add data food security
+fs_df_data <-dat$fs_df_data
+fs_beneficiary_types <- fs_df_data %>% 
+  filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique() %>% sort()
+
+els_df_data <-dat$els_df_data
+els_beneficiary_types <- els_df_data %>% 
+  filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()%>% sort()

f0bfc3946f07bfe070e6264cb78421a4f8b03c57
twesigye10 <twesigye10@gmail.com>
2021-04-29 12:04:58 +0300

    EPR: data

--- modules/import-data.R
+++ modules/import-data.R
@@ -1,19 +1,28 @@
 # Data --------------------------------------------------------------------
 
 display_in_title <- " for all Districts"
-# add data CBI
+# add data: All
 dat<-read_rds(file = "data/data.rds")
-df_data<- dat$df_data
+
+# add data: Shapefile
 df_shape<- dat$df_shape
 
+# add data: CBI
+df_data<- dat$df_data
 beneficiary_types <- df_data %>% 
   filter(!is.na(Select_Beneficiary_Type)) %>% pull(Select_Beneficiary_Type) %>% unique() %>% sort()
 
-# add data food security
+# add data: food security
 fs_df_data <-dat$fs_df_data
 fs_beneficiary_types <- fs_df_data %>% 
   filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique() %>% sort()
 
+# add data: livelihood
 els_df_data <-dat$els_df_data
 els_beneficiary_types <- els_df_data %>% 
+  filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()%>% sort()
+
+# add data: environmental protection and restoration
+epr_df_data <-dat$epr_df_data
+epr_beneficiary_types <- epr_df_data %>% 
   filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()%>% sort()

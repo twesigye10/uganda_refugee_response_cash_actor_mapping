720e5ff365888c76f90628e91ccc56759685c508
twesigye10 <twesigye10@gmail.com>
2021-05-07 12:10:54 +0300

    Adding WASH datasets

--- creating_project_data.R
+++ creating_project_data.R
@@ -19,7 +19,9 @@ library(janitor)
 # currency conversion
 currency_conversion_factor <- 1000
 display_in_title <- " For All districts"
-# CBI data
+
+# CBI data ----------------------------------------------------------------
+
 cbi_df_data <- read_csv(file = "data/RRP_5W_CBI_for_basic_needs_20210305_055004_UTC.csv")
 cbi_df_data <- janitor::clean_names(cbi_df_data) %>% 
     separate(select_month, c("Month", "Year"), "-", remove= FALSE, extra = "drop") %>% 
@@ -46,7 +48,9 @@ cbi_beneficiary_types <- cbi_df_data %>%
 df_shape <- st_read("data/UGA_Admin/UGA_Admin_2_Districts_2020.shp", crs=4326 ) %>% 
     mutate(ADM2_EN = toupper(ADM2_EN))
 
-# Food security data
+
+# Food security data ------------------------------------------------------
+
 df_food_security <- read_csv("data/Food_Security.csv")
 df_food_security <- janitor::clean_names(df_food_security) %>% 
     mutate(
@@ -64,6 +68,9 @@ fs_df_data <- df_food_security %>%
 
 fs_beneficiary_types <- fs_df_data %>% 
     filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()
+
+
+# Emergency Livelihood Support --------------------------------------------
 
 # Increased access to short-term employment opportunities data
 seo_df_data <- read_csv("data/ELS_Increased_access_to_short-term_employment_opportunities.csv")
@@ -104,7 +111,9 @@ apa_beneficiary_types <- apa_df_data %>%
     filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()
 
 
-# Environment Protection data
+
+# Environment Protection data ---------------------------------------------
+
 df_environment_protection_restoration <- read_csv("data/Forests_wetlands_shorelines_protected_and_restored.csv")
 df_environment_protection_restoration <- janitor::clean_names(df_environment_protection_restoration) %>% 
     mutate(
@@ -124,7 +133,54 @@ epr_df_data <- df_environment_protection_restoration %>%
 epr_beneficiary_types <- epr_df_data %>% 
     filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()
 
-# saving several data objects into an RDS object
+
+
+# WASH data ---------------------------------------------------------------
+
+# CBI approach in sanitation services data
+ss_df_data <- read_csv("data/5W_CBI_approach_in_sanitation_services.csv")
+ss_df_data <- janitor::clean_names(ss_df_data) %>% 
+    separate(end_date, c("Year", "Month"), "-", remove= FALSE, extra = "drop") %>% 
+    mutate(
+        total_cash_value_of_cash_grants_ugx = ifelse(!is.na(total_cash_value_of_cash_grants_ugx), (total_cash_value_of_cash_grants_ugx/currency_conversion_factor), NA),
+        Quarter = case_when(Month %in% c("01", "02", "03")~"Q1",
+                            Month %in% c("04", "05", "06")~"Q2",
+                            Month %in% c("07", "08", "09")~"Q3",
+                            Month %in% c("10", "11", "12")~"Q4"  )
+    )%>% 
+    filter(!is.na(end_date) & !is.na(delivery_mechanism) & !is.na(partner_name)) %>% 
+    filter(
+        !grepl("^[0-9]", delivery_mechanism,  ignore.case = FALSE) & 
+            !grepl("^[0-9]", partner_name,  ignore.case = FALSE) & 
+            total_cash_value_of_cash_grants_ugx > 0
+    )
+
+ss_beneficiary_types <- ss_df_data %>% 
+    filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()
+
+# CBI approach in WASH NFI data
+wn_df_data <- read_csv("data/5W_CBI_approach_in_WASH_NFI.csv")
+wn_df_data <- janitor::clean_names(wn_df_data) %>% 
+    separate(end_date, c("Year", "Month"), "-", remove= FALSE, extra = "drop") %>% 
+    mutate(
+        total_cash_value_of_cash_grants_ugx = ifelse(!is.na(total_cash_value_of_cash_grants_ugx), (total_cash_value_of_cash_grants_ugx/currency_conversion_factor), NA),
+        Quarter = case_when(Month %in% c("01", "02", "03")~"Q1",
+                            Month %in% c("04", "05", "06")~"Q2",
+                            Month %in% c("07", "08", "09")~"Q3",
+                            Month %in% c("10", "11", "12")~"Q4"  )
+    )%>% 
+    filter(!is.na(end_date) & !is.na(delivery_mechanism) & !is.na(partner_name)) %>% 
+    filter(
+        !grepl("^[0-9]", delivery_mechanism,  ignore.case = FALSE) & 
+            !grepl("^[0-9]", partner_name,  ignore.case = FALSE) & 
+            total_cash_value_of_cash_grants_ugx > 0
+    )
+
+wn_beneficiary_types <- wn_df_data %>% 
+    filter(!is.na(select_beneficiary_type)) %>% pull(select_beneficiary_type) %>% unique()
+
+# saving several data objects into an RDS object --------------------------
+
 data_for_saving <- list()
 data_for_saving$cbi_df_data <- cbi_df_data
 data_for_saving$df_shape <- df_shape
@@ -132,6 +188,8 @@ data_for_saving$fs_df_data <- fs_df_data
 data_for_saving$seo_df_data <- seo_df_data
 data_for_saving$apa_df_data <- apa_df_data
 data_for_saving$epr_df_data <- epr_df_data
+data_for_saving$ss_df_data <- ss_df_data
+data_for_saving$wn_df_data <- wn_df_data
 
 saveRDS(data_for_saving, file = "data/new_data.rds")
 

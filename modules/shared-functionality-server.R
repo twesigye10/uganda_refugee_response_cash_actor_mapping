e23a68d2785b4f2a908756ffc652f1d479b2ad36
twesigye10 <twesigye10@gmail.com>
2021-05-20 20:08:55 +0300

    Added shared functionality for creating dynamicMapLayer, refugeeHostLayer, and dynamicMapLabels

--- modules/shared-functionality-server.R
+++ modules/shared-functionality-server.R
@@ -63,3 +63,102 @@ dfShapeDefault <- function(id, input_shape_data, input_cash_data, input_field_di
     )
   })
 }
+
+# dynamic map layer module ------------------------------------------------------
+dynamicMapLayer <- function(id, input_map, input_data){
+  moduleServer(id, function(input, output, session){
+    # Create a continuous palette function
+    pal <- colorNumeric(
+      palette = "Reds",
+      domain = input_data$cash_transfers_by_district,
+      na.color = "#b6b6b7"
+    )
+    # label districts in the map
+    labels_v1 <- ~sprintf(
+      "<strong>%s</strong><br/>Cash Transfers : %s ",
+      stringr::str_to_title(ADM2_EN), cash_transfers_by_district
+    ) %>% 
+      lapply(htmltools::HTML)
+    
+    # construct the dynamic map
+    proxy = leafletProxy(input_map, data = input_data)  
+    proxy %>% 
+      clearControls() %>% 
+      addPolygons(
+        color = "white",
+        options = pathOptions(
+          clickable = ~ifelse(!is.na(cash_transfers_by_district), TRUE, FALSE)),
+        fillColor = ~pal(cash_transfers_by_district),
+        fillOpacity = ~ifelse(is.na(cash_transfers_by_district), 0.7, 1),
+        weight = 1,
+        opacity = 1,
+        label = labels_v1,
+        labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"),
+                                    textsize = "15px",
+                                    direction = "auto", opacity =0.75),
+        layerId = ~ADM2_EN,
+        dashArray = "3",
+        highlight = highlightOptions(weight = 3,
+                                     color = "#666",
+                                     dashArray = "",
+                                     fillOpacity = 0.7,
+                                     bringToFront = TRUE),
+        group="Districts Assessed"
+      ) %>% 
+      addLegend(position ="bottomright", 
+                pal = pal, 
+                values = ~cash_transfers_by_district,
+                title = "Total cash<br>(UGX '000)",
+                opacity  = 1,
+                na.label = "<span class = \"nowrap\">Not Assessed </span>"
+      )
+  })
+}
+
+# Refugee hosting districts layer -----------------------------------------
+refugeeHostLayer <- function(id, input_map, input_data){
+  moduleServer(id, function(input, output, session){
+    # Create a continuous palette function
+    pal <- colorFactor( c("#A1BCD7", "#b6b6b7"), levels = c("Host", "None Host")
+    )
+    # construct the dynamic map
+    proxy = leafletProxy(input_map, data = input_data)  
+    proxy %>% 
+      # clearControls() %>% 
+      addPolygons(
+        color = "white",
+        options = pathOptions(clickable = FALSE),
+        fillColor = ~pal(col_legenend_factor),
+        fillOpacity = 0.7,
+        weight = 1,
+        opacity = 0.7,
+        layerId = ~ADM2_EN,
+        dashArray = "3"
+      ) %>% 
+      addLegend(position ="bottomright",
+                pal = pal,
+                values = ~col_legenend_factor,
+                title = "Refugee<br>Host Status",
+                opacity  = 0.7
+      )
+  })
+}
+
+# dynamic map labels module ------------------------------------------------------
+dynamicMapLabels <- function(id, input_map, input_data){
+  moduleServer(id, function(input, output, session){
+    # label districts in the map
+    labels_district <- ~sprintf(
+      "<strong>%s</strong>",
+      stringr::str_to_title(ADM2_EN)
+    ) %>% 
+      lapply(htmltools::HTML)
+    # add labels on the map
+    proxy = leafletProxy(input_map, data=input_data ) 
+    proxy %>%
+      clearMarkers() %>%
+      addLabelOnlyMarkers( label = labels_district, 
+                           labelOptions = labelOptions(noHide = T, textOnly = TRUE)
+      )
+  })
+}

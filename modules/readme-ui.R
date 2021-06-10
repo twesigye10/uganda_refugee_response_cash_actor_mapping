9dc3b5254dd2b26944957962f9a9ba8565ed5198
Anthony Twesigye <twesigye10@gmail.com>
2021-06-10 20:53:34 +0300

    modified readme

--- modules/readme-ui.R
+++ modules/readme-ui.R
@@ -3,18 +3,19 @@
 readmeUI <- function(id){
   
               div(class="read-me-div",
-                  h2("About"),
+                  h4("Cash-Based Interventions (CBI), Uganda Refugee Response Plan (RRP)"),
+                  h5("About"),
                   div(class="page-text",
                       p( span("This dashboard offers information on the who, what, when, and where (4Ws) 
                       of cash-based interventions in Uganda. It was developed by REACH Uganda, in collaboration with 
-                      UNHCR and the Uganda Cash Working Group, with funding from USAID's Bureau for Humanitarian Assistance. 
+                      United Nations High Commissioner for Refugees (UNHCR) and the Uganda Cash Working Group, with funding from United States Agency for International Development (USAID's) Bureau for Humanitarian Assistance. 
                       The information presented here is based on partner activities as reported to the Activity Info platform 
                       from the first quarter in 2019 until present. Data and information displayed in the dashboard is updated 
                       on a quarterly basis following the Activity Info reporting cycle. Partners can update or submit new data 
-                      about cash related activities they have implemented by following this "), strong( a("ActivityInfo.", style="color:#333", href ="https://www.activityinfo.org/login?page=%23database%2Fd0000010295%2Ff0000022161")), 
+                      about cash related activities they have implemented by following this "), strong( a("ActivityInfo.", style="color:#0f69b6", href ="https://www.activityinfo.org/login?page=%23database%2Fd0000010295%2Ff0000022161")), 
                       span("For inquiries on how to submit data to the platform please contact (UNHCR focal person)."))
                   ),
-                  h2("How it works"),
+                  h5("How it works"),
                   
                   div(class="page-text",
                       p("The tabs at the top of the page allow you to view data on cash-based interventions within that sector. 
@@ -26,10 +27,10 @@ readmeUI <- function(id){
                       p("You can also filter and view data by year or quarter."),
                       p("After you have selected a particular district and you wish to return, 
                       click the National Overview button to reset the map and indicators. ")),
-                  h2("Contact"),
+                  h5("Contact"),
                   div(class="page-text",
                       p( span("If you have comments or feedback on this dashboard, 
-                      please share them with us at "), strong(a(href= "mailto:uganda@reach-initiative.org", "REACH", style="color:#333")) )
+                      please share them with us at "), strong(a(href= "mailto:uganda@reach-initiative.org", "REACH", style="color:#0f69b6")) )
                   ),
                   div(class = "page-partners", img(height = 50, width = 40, src="LogoOPM.png"),
                                img(height = 40, width = 100, src="reach-logo-informing.png"),
